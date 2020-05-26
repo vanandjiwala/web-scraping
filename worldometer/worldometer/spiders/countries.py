b685d7005d66ae20ce1c421e49eafe1917e9d5d3
Vasav Anandjiwala <vasav.anandjiwala@gmail.com>
2020-05-26 16:41:14 +0530

    Initial Commit - Simple spider.

---
+++ worldometer/worldometer/spiders/countries.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class CountriesSpider(scrapy.Spider):
+    name = 'countries'
+    allowed_domains = ['www.worldometers.info/world-population/population-by-country']
+    start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
+
+    def parse(self, response):
+        countries = response.xpath("//td/a/text()").getall()
+
+        yield {
+            'countries': countries
+        }
